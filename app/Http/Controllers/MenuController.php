884ffdf264010c1a503ab6c5d628ae9a2a108907
A <1207074@student.roc-nijmegen.nl>
2025-04-15 11:54:24 +0200

    Actual Commit

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Menu;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $menus = Menu::all();
+        return view('menu.index', compact('menus'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('menu.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate( [
+            "name" => "required",
+            "price" => "required|numeric",
+            "description" => "required",
+        ]);
+
+        Menu::create($request->all());
+        return redirect()->route('menu.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Menu $menu)
+    {
+        return view('menu.show', compact('menu'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Menu $menu)
+    {
+        return view('menu.edit', compact('menu'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Menu $menu)
+    {
+        $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'description' => 'required',
+        ]);
+
+        $menu->update($request->all());
+        return redirect()->route('menu.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Menu $menu)
+    {
+        $menu->delete();
+        return redirect()->route('menu.index');
+    }
+}
